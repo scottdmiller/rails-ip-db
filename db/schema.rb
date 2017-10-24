b8a9ec36adf1d1bbe0da33400b3cbf62d0d4a3a5
Scott Miller <sxm77@psu.edu>
2017-10-24 13:34:09 -0400

    VSCode has an opinion on formatting and I can't handle having warnings.

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,10 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20171024145647) do
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'users', force: :cascade do |t|
+    t.string 'name'
+    t.string 'email'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
-
 end
