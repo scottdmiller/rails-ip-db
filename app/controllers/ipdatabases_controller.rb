2c42446314b51af2b7dd07322942ed94be36bcbb
Scott Miller <sxm77@psu.edu>
2017-10-25 13:05:32 -0400

    update contoller to match class adding fields to _form adding table to index

--- app/controllers/ipdatabases_controller.rb
+++ app/controllers/ipdatabases_controller.rb
@@ -2,6 +2,6 @@ class IpdatabasesController < ApplicationController
   before_action only: [:show, :edit, :update, :destroy]
 
   def index
-    @ip_address = ip_address.all
+    @ip_address = Ipdatabase.all
   end
 end
